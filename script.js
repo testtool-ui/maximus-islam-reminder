28f97dc2ca1767a6f119347f1b4a2151e68508b3
testtool-ui <godlikegamer9394@gmail.com>
2024-12-18 12:35:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Smooth scrolling for the "Reflect on the Lessons" button
 document.querySelector('.btn').addEventListener('click', function(e) {
     e.preventDefault();
     document.querySelector('#reminder').scrollIntoView({
